cec2681d31f177a3d964a644579ac8caee6f6412
Silke pilon <silkepilon2009@gmail.com>
2026-02-02 11:29:22 +0100

    feat: convert audience ID field to dynamic dropdown

--- nodes/Resend/actions/audience/get.operation.ts
+++ nodes/Resend/actions/audience/get.operation.ts
@@ -1,29 +1,29 @@
 import type { INodeProperties, IExecuteFunctions, INodeExecutionData } from 'n8n-workflow';
 import { apiRequest } from '../../transport';
+import { createDynamicIdField, resolveDynamicIdValue } from '../../utils/dynamicFields';
 
 export const description: INodeProperties[] = [
-	{
-		displayName: 'Audience ID',
-		name: 'audienceId',
-		type: 'string',
+	...createDynamicIdField({
+		fieldName: 'audienceId',
+		resourceName: 'audience',
+		displayName: 'Audience',
 		required: true,
-		default: '',
 		placeholder: 'aud_123456',
+		description: 'The audience to retrieve',
 		displayOptions: {
 			show: {
 				resource: ['audiences'],
 				operation: ['get'],
 			},
 		},
-		description: 'The unique identifier of the audience to retrieve. Obtain from the Create Audience response or List Audiences operation. Returns audience details including name and creation date.',
-	},
+	}),
 ];
 
 export async function execute(
 	this: IExecuteFunctions,
 	index: number,
 ): Promise<INodeExecutionData[]> {
-	const audienceId = this.getNodeParameter('audienceId', index) as string;
+	const audienceId = resolveDynamicIdValue(this, 'audienceId', index);
 
 	const response = await apiRequest.call(
 		this,
